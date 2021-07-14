e70eebcc5baba2bd7f54115f6bb8925b6ee0ed56
1A-LITHIUM\gate <gate.jocelyn@gmail.com>
2021-07-14 12:04:51 +0200

    no message

--- photobooth.py
+++ photobooth.py
@@ -1281,7 +1281,7 @@ class MainWindow(QMainWindow):
             c = color
             if color1 is not None:
                 c = color1
-            painterFrame.fillRect(0, h - h / 5, w / 3 + 50, h / 5 + 1, c)
+            painterFrame.fillRect(0, h - h / 5, w / 3 + 60, h / 5 + 1, c)
 
         if hideButton2 is True:
             c = color
